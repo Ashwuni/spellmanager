eadb43eb082e06be668c1f5d65eff679544e7416
Ashwuni Kumar <a255kuma@uwaterloo.ca>
2020-08-04 05:07:13 +0000

    Commit

--- db/migrate/20200804042904_create_spells.rb
+++ db/migrate/20200804042904_create_spells.rb
@@ -1,7 +1,13 @@
 class CreateSpells < ActiveRecord::Migration[6.0]
   def change
     create_table :spells do |t|
-      t.string :name
+      t.string :name, limit: 100, null: false
+      t.references :book, foreign_key: true
+#       level
+#       school
+#       classes
+#       concentration
+      t.string :description, limit: 4096
 
       t.timestamps
     end
