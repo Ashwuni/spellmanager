48bb774dff889218b56e6e9daced49e69e147f70
Ashwuni Kumar <a255kuma@uwaterloo.ca>
2020-08-10 03:48:20 +0000

    Commirt

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,29 +1,22 @@
 class CharactersController < ApplicationController
-
-  # GET /authors
   def index
     @characters = Character.all
   end
 
-  # GET /authors/:id
   def show
     @character = Character.find(params[:id])
   end
 
-  # GET /authors/new
   def new
     @character = Character.new
   end
 
-  # GET /authors/:id/edit
   def edit
     @character = Character.find(params[:id])
   end
 
-  # POST /authors
   def create
-    # active record new, create, and update allow us to pass in a hash with the
-    # attributes and their values, author_params is a method (see below!)      
+    
     @character = Character.new(character_params)
 
     if @character.save
@@ -34,7 +27,6 @@ class CharactersController < ApplicationController
     end 
   end
 
-  # PATCH/PUT /authors/:id
   def update
     @character = Character.find(params[:id])      
     if @character.update(character_params)
@@ -45,13 +37,10 @@ class CharactersController < ApplicationController
     end
   end
 
-  # GET /authors/:id/confirm_delete  
   def confirm_delete
       @character = Character.find(params[:id])
-      # show the form for confirm_delete of user 
   end        
-    
-  # DELETE /authors/:id
+ 
   def destroy
     @character = Character.find(params[:id])  
     if @character.num_books>0
@@ -88,12 +77,7 @@ class CharactersController < ApplicationController
   end
 
   private
-    # Only allow a trusted parameter "white list" through.
     def character_params
-      # params is a hashtable.  It should have in it a key of :author.
-      # The value for the :author key is another hash.
-      # If params does not contain the key :author, an exception is raised.  
-      # Only the "author" hash is returned and only with the permitted key(s).
       params.require(:character).permit(:class, :class)
     end
 end
--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -1,31 +1,22 @@
 class SpellsController < ApplicationController
 
-  # GET /spells
   def index
-    # We've added a method, self.order_by, to the Book model, 
-    # see models/book.rb
     @spells = Spell.order_by params[:order_by]
   end
 
-  # GET /books/:id
   def show
     @spell = Spell.find(params[:id])
   end
 
-  # GET /books/new
   def new
     @spell = Spell.new
   end
 
-  # GET /books/:id/edit
   def edit
     @spell = Spell.find(params[:id])
   end
 
-  # POST /books
   def create
-    # active record new, create, and update allow us to pass in a hash with the
-    # attributes and their values, book_params is a method (see below!)
     @spell = Spell.new(spell_params)
 
     if @spell.save
@@ -35,8 +26,7 @@ class SpellsController < ApplicationController
       render :new
     end
   end
-
-  # PATCH/PUT /books/:id
+    
   def update
     @spell = Spell.find(params[:id])  
     if @spell.update(spell_params)
@@ -47,7 +37,6 @@ class SpellsController < ApplicationController
     end
   end
 
-  # DELETE /books/:id
   def destroy
     @spell = Spell.find(params[:id])  
     if @spell.destroy
@@ -59,13 +48,7 @@ class SpellsController < ApplicationController
   end
 
   private
-    # Only allow a trusted parameter "white list" through.
     def spell_params
-      # params is a hashtable.  It should have in it a key of :book.
-      # The value for the :book key is another hash.
-      # If params does not contain the key :book, an exception is raised.  
-      # Only the "book" hash is returned and only with the permitted key(s).
-      # So returns a hash with only having at most keys of :title, :year, :author_id
       params.require(:spell).permit(:name, :level, :creater_id)
     end
       
